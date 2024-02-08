956821a00ed8dabcc338a4f7bd494c28db71b98d
Sam-MARTis <samanthmartis@gmail.com>
2024-02-08 19:20:54 +0530

    feat: added dictionary reading

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
+const arr = [
+    {'type': 'other', 'text': 'Hello world'},
+    {'type': 'self', 'text': 'world is small'},
+    {'type': 'self', 'text': 'Hello universe'},
+    {'type': 'other', 'text': 'shuddup'},
+];
 
+window.onload = () => {
+    for(let i = 0; i < arr.length; i++) {
+        let current = arr[i];
+        if(current.type === 'self') {
+            addTextBox(current.text, 1);
+        }
+        else if(current.type === 'other') {
+            addTextBox(current.text, 0);
+
+        }
+    }
+}
 const submitText = (state) => {
     textArea = document.getElementById('textArea');
     text = textArea.value;
